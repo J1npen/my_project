dd5bec240b3871ba9e070fef312a38f44113b971
Jinpen <kx180415@gmail.com>
2025-03-03 00:21:57 +0800

    Initial commit

---
+++ learn_replace.py
@@ -0,0 +1,6 @@
+message_1 = "Hello Python world!"
+print(message_1.replace("Python", "C"))
+message_2 = "Hello Python world!"
+print(message_2.replace("Python", "C++"))
+message_3 = "Hello Python world!"
+print(message_3.replace("Python", "Java"))
